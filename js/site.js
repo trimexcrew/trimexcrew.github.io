c8e03fbfc24562c024e51a088ececce234980e5d
Roy Macdonald <macdonald.roy@gmail.com>
2018-10-26 00:34:13 -0300

    agregados scripts en js. Lightbox

---
+++ js/site.js
@@ -0,0 +1,16 @@
+
+//---------------------------------------------------------------
+$(document).ready(function(){
+$('.post-content img').each(function( index ) {
+$( this ).wrap("<a class=\"post-img\" href=\"" + $( this ).attr("src")+ "\" data-lightbox=\"img-set\" data-title=\""+ $( this ).attr("alt") +"\"></a>");});
+
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+$(function() {
+     lightbox.option({
+      'resizeDuration': 200,
+      'fadeDuration': 300,
+      'wrapAround': true
+    });
+});
+//---------------------------------------------------------------
