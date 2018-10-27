2632a3bc354f8581deb413e6cce4557fa889dc01
Roy Macdonald <macdonald.roy@gmail.com>
2018-10-27 19:34:29 -0300

    lightbox actualizado a version mas nueva 2.10

--- js/site.js
+++ js/site.js
@@ -4,13 +4,11 @@ $(document).ready(function(){
 $('.post-content img').each(function( index ) {
 $( this ).wrap("<a class=\"post-img\" href=\"" + $( this ).attr("src")+ "\" data-lightbox=\"img-set\" data-title=\""+ $( this ).attr("alt") +"\"></a>");});
 
-//---------------------------------------------------------------
-//---------------------------------------------------------------
-$(function() {
-     lightbox.option({
-      'resizeDuration': 200,
-      'fadeDuration': 300,
-      'wrapAround': true
-    });
+lightbox.option({
+ 'resizeDuration': 200,
+ 'fadeDuration': 300,
+ 'wrapAround': true
 });
+});
+//---------------------------------------------------------------
 //---------------------------------------------------------------
