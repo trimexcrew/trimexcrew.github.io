40a04fc841d226fc48662e6c72f6844df0a9660c
Roy Macdonald <macdonald.roy@gmail.com>
2018-12-06 23:43:16 -0300

    cleanup a site.js

--- js/site.js
+++ js/site.js
@@ -1,8 +1,9 @@
 
 //---------------------------------------------------------------
 $(document).ready(function(){
-$('.post-content img').each(function( index ) {
-$( this ).wrap("<a class=\"post-img\" href=\"" + $( this ).attr("src")+ "\" data-lightbox=\"img-set\" data-title=\""+ $( this ).attr("alt") +"\"></a>");});
+// $('.post-content img').each(function( index ) {
+// $( this ).wrap("<a class=\"post-img\" href=\"" + $( this ).attr("src")+ "\" data-lightbox=\"img-set\" data-title=\""+ $( this ).attr("alt") +"\"></a>");
+// });
 
 lightbox.option({
  'resizeDuration': 200,
@@ -13,7 +14,7 @@ AOS.init();
 
 $('.mas_abajo').on('click', function(e) {
       $('html, body').animate({
-        scrollTop: $(".posts").offset().top
+        scrollTop: $("#ig").offset().top
       }, 800);
     });
 
