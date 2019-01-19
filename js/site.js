0b46906b5f6559674bddc81a15c55393be2e4429
Roy Macdonald <macdonald.roy@gmail.com>
2019-01-18 21:41:11 -0300

    limpieza de codigo innecesario, lo que quedo despues de sacar las descripciones de los posts en el home

--- js/site.js
+++ js/site.js
@@ -1,10 +1,6 @@
 
 //---------------------------------------------------------------
 $(document).ready(function(){
-// $('.post-content img').each(function( index ) {
-// $( this ).wrap("<a class=\"post-img\" href=\"" + $( this ).attr("src")+ "\" data-lightbox=\"img-set\" data-title=\""+ $( this ).attr("alt") +"\"></a>");
-// });
-
     lightbox.option({
       'resizeDuration': 200,
       'fadeDuration': 300,
@@ -17,13 +13,6 @@ $(document).ready(function(){
       $('html, body').animate({
         scrollTop: $("#ig").offset().top
       }, 800);
-    });
-
-    $('.ig_grid_line').children('.grid-post-content').css('display', 'none');
-    $('.ig_grid_line').mouseenter( function(e) {
-      $(this).children('.grid-post-content').css('display', 'flex');
-    }).mouseleave(function(e) {
-      $(this).children('.grid-post-content').css('display', 'none');
     });
 });
 //---------------------------------------------------------------
